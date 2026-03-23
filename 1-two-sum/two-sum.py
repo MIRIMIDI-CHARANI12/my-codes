d06beb941310c8da06ac76ddf0fe880c679a01bf
MIRIMIDI CHARANI <charinimirimidi006@gmail.com>
2026-03-23 19:32:49 +0530

    Time: 1778 ms (11.00%) | Memory: 19.8 MB (79.06%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(n):
             for j in range(i+1,n):
                 if nums[i]+nums[j]==target:
-                    return[i,j]
-                    
+                    return[i,j]            
         return[]
         
